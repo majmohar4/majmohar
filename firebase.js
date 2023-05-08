52eef2632e9782cc060e83b8074b37d63a5c3630
makovka44 <maj.mohar4@gmail.com>
2023-05-08 18:45:04 +0200

    revert + dodan password repeat

--- firebase.js
+++ firebase.js
@@ -63,8 +63,16 @@ function login() {
 
 // Sign up function
 function signup() {
-    const email = document.getElementById("signup-email").value;
-    const password = document.getElementById("signup-password").value;
+    console.log("prevented")
+    const loginForm = document.getElementById("login-form");
+    loginForm.addEventListener("submit", (event) => {
+      event.preventDefault(); // prevent default form submission behavior
+    }); 
+    const email = document.getElementById("login-email").value;
+    const password = document.getElementById("login-password").value;
+    const repeat_password = document.getElementById("repeat-password").value;
+    console.log(password + " in " + repeat_password)
+    if (password === repeat_password){
     auth.createUserWithEmailAndPassword(email, password)
         .then((userCredential) => {
             const user = userCredential.user;
@@ -81,6 +89,9 @@ function signup() {
             alert(error.message);
             alert("Account not created! Try again.")
         });
+    }else{
+        alert("Passwords do not match.");
+    }
 }
 
 // Logout function
@@ -110,61 +121,4 @@ function getUsers() {
         .catch(error => {
             alert(error.message);
         });
-}
-function signInWithGitHub() {
-    const loginForm = document.getElementById("login-form");
-    loginForm.addEventListener("submit", (event) => {
-      event.preventDefault(); // prevent default form submission behavior
-    }); 
-    const clientId = 'eee8e5694fcd93a98188'; // Replace with your own client ID
-    const redirectUri = 'https://majmohar.me/login.html'; // Replace with your own redirect URI
-    const scope = 'read:user'; // Replace with the scopes you need
-
-    const url = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}&state=STATE`;
-    window.location.href = url;
-  }
-
-  function getUserProfile(accessToken) {
-    fetch('https://api.github.com/user', {
-      headers: {
-        'Authorization': `Bearer ${accessToken}`
-      }
-    })
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-      document.getElementById("user-login").innerHTML = data.login;
-      document.getElementById("user-id").innerHTML = data.id;
-      document.getElementById("user-avatar").src = data.avatar_url;
-      document.getElementById("profile").style.display = "block";
-    })
-    .catch(error => {
-      alert(error.message);
-    });
-  }
-
-  const urlParams = new URLSearchParams(window.location.search);
-  const code = urlParams.get('code');
-  if (code) {
-    fetch('https://github.com/login/oauth/access_token', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        client_id: 'eee8e5694fcd93a98188', // Replace with your own client ID
-        client_secret: 'd4c88530da139d6b919115072bed31cb5223b198', // Replace with your own client secret
-        code: code,
-        redirect_uri: 'https://majmohar.me/login.html' // Replace with your own redirect URI
-      })
-    })
-    .then(response => response.text())
-    .then(data => {
-      console.log(data);
-      const accessToken = new URLSearchParams(data).get('access_token');
-      getUserProfile(accessToken);
-    })
-    .catch(error => {
-      alert(error.message);
-    });
 }
